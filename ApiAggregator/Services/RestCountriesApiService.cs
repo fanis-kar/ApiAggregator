5ce6c302fd4bf3711988908061cf873136d2ae19
Theofanis Karamichalelis <fanis.karamichalelis@gmail.com>
2024-12-28 11:45:35 +0200

    Remove empty spaces

--- ApiAggregator/Services/RestCountriesApiService.cs
+++ ApiAggregator/Services/RestCountriesApiService.cs
@@ -3,52 +3,51 @@ using ApiAggregator.Services.Interfaces;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 
-namespace ApiAggregator.Services
+namespace ApiAggregator.Services;
+
+public class RestCountriesApiService : IRestCountriesApiService
 {
-    public class RestCountriesApiService : IRestCountriesApiService
+    private readonly HttpClient _httpClient;
+    private readonly string _apiUrl;
+    private readonly ILogger<RestCountriesApiService> _logger;
+
+    public RestCountriesApiService(HttpClient httpClient, 
+            IOptions<ExternalApiSettings> options,
+            ILogger<RestCountriesApiService> logger)
     {
-        private readonly HttpClient _httpClient;
-        private readonly string _apiUrl;
-        private readonly ILogger<RestCountriesApiService> _logger;
+        _logger = logger;
+        _httpClient = httpClient;
+        _httpClient.DefaultRequestHeaders.Add("User-Agent", "ApiAggregation/1.0");
 
-        public RestCountriesApiService(HttpClient httpClient, 
-                IOptions<ExternalApiSettings> options,
-                ILogger<RestCountriesApiService> logger)
+        var settings = options.Value.RestCountriesApi;
+        _apiUrl = settings.Url ?? string.Empty;
+    }
+
+    public async Task<RestCountriesApiResponse> GetData(string query)
+    {
+        if (string.IsNullOrEmpty(query)) query = "euro";
+        var requestUrl = $"{_apiUrl}{query}";
+        try
         {
-            _logger = logger;
-            _httpClient = httpClient;
-            _httpClient.DefaultRequestHeaders.Add("User-Agent", "ApiAggregation/1.0");
+            var response = await _httpClient.GetAsync(requestUrl);
 
-            var settings = options.Value.RestCountriesApi;
-            _apiUrl = settings.Url ?? string.Empty;
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var countries = JsonConvert.DeserializeObject<List<Country>>(content);
+
+            return new RestCountriesApiResponse { Countries = countries ?? [] };
         }
-
-        public async Task<RestCountriesApiResponse> GetData(string query)
+        catch (HttpRequestException ex)
         {
-            if (string.IsNullOrEmpty(query)) query = "euro";
-            var requestUrl = $"{_apiUrl}{query}";
-            try
-            {
-                var response = await _httpClient.GetAsync(requestUrl);
-
-                response.EnsureSuccessStatusCode();
-
-                var content = await response.Content.ReadAsStringAsync();
-
-                var countries = JsonConvert.DeserializeObject<List<Country>>(content);
-
-                return new RestCountriesApiResponse { Countries = countries ?? [] };
-            }
-            catch (HttpRequestException ex)
-            {
-                _logger.LogError($"RestCountriesApi error: {ex.Message}");
-                return new RestCountriesApiResponse();
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Unexpected error fetching RestCountriesApi data: {ex.Message}");
-                return new RestCountriesApiResponse();
-            }
+            _logger.LogError($"RestCountriesApi error: {ex.Message}");
+            return new RestCountriesApiResponse();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Unexpected error fetching RestCountriesApi data: {ex.Message}");
+            return new RestCountriesApiResponse();
         }
     }
 }
